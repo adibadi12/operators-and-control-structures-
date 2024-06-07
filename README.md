7fa59dd655501603cf04908c342b8293bc44893b
adibadi12 <165892521+adibadi12@users.noreply.github.com>
2024-06-07 14:50:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# operators-and-control-structures-
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/adibadi12/operators-and-control-structures-)
---
+++ script.js
@@ -0,0 +1,16 @@
+
+//Conditional statements
+let num = 33;
+if (num > 0){
+  console.log('positive');
+} else if (num < 0){
+  console.log('negative');
+}else {
+  console.log('Zero');
+}
+
+//
+
+
+
+
