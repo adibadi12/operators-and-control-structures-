4894c7571b7c7c2287e489d85d65e13294fdd480
adibadi12 <adrian.mathiassen@gmail.com>
2024-06-07 16:21:16 +0200

    some more console.logs

--- script.js
+++ script.js
@@ -9,7 +9,50 @@ if (num > 0){
   console.log('Zero');
 }
 
-//
+// Looping statements
+for(let i=0; i<5; i++){
+  console.log(i);
+}
+
+// Jump statements
+let fruits = ['Apple', 'Banana', 'Orange'];
+for(let fruit of fruits){
+  if(fruit=== 'Banana'){
+    break;
+  }
+  console.log(fruit);
+}
+
+for(let i = 0; i < 10; i++){
+  console.log(i);
+  if (i===5){
+   continue;
+  }
+}
+
+let x = 33;
+let y = 3;
+let sum = x * y;
+console.log (sum)
+
+let a = 12;
+let K = 4;
+let isEqual = (a==K);
+console.log(isEqual)
+
+let OnepieceIsBetter = true;
+let NarutoIsBetter = false;
+let TheTruth = OnepieceIsBetter && NarutoIsBetter;
+console.log (TheTruth)
+
+let G = 56;
+let isNegative = -G;
+console.log (isNegative);
+
+let age=19;
+let status = (age>= 18) ? 'Adult' : 'Minor';
+console.log(status);
+
 
 
 
